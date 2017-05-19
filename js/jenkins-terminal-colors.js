b84b1c464c72843477f98d83cb63d805c84cc245
Adam Dierkens <Adam_Dierkens@intuit.com>
2017-05-19 08:33:15 -0700

    Fix the plugin

--- js/jenkins-terminal-colors.js
+++ js/jenkins-terminal-colors.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
     function transformText(text) {
-        text = text.replace(/\[0m/g, "</span>");
-        text = text.replace(/\[([0-9;]+)m/g, function (match, code, offset, string) {
-            return "<span class='jtc-" + code.replace(/;/g, ' jtc-') + "'>";
-        });
 
-        return text;
+        console.log(text);
+
+        // text = text.replace(/\[m/g, "</span>");
+        // text = text.replace(/\[([0-9;]+)m/g, function (match, code, offset, string) {
+        //     return "<span class='jtc-" + code.replace(/;/g, ' jtc-') + "'>";
+        // });
+
+        var ansi_up = new AnsiUp;
+        return ansi_up.ansi_to_html(text);
     }
 
     function reformatExecute() {
         var pre_element = $('pre').get(0);
 
-        var original_inner_html = pre_element.innerHTML;
+        var original_inner_html = $("<p/>").html(pre_element.innerHTML).text();
         var reformated_inner_html = transformText(original_inner_html);
 
         // Only update the content if it actually changed.
@@ -21,5 +25,7 @@ $(document).ready(function() {
             pre_element.innerHTML = reformated_inner_html;
         }
     }
-    setInterval(reformatExecute, 200);
+    // setInterval(reformatExecute, 200);
+    window.reformatExecute = reformatExecute;
+    reformatExecute();
 });
